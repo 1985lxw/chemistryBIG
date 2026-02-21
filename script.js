0c8ab40defb19be762def712d6d3b66b7c13870e
Keith Miller <kmiller4@wpi.edu>
2026-02-21 16:55:55 -0500

    moved particle class to classes/particle.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { Environment } from "./environment.js";
+import { Particle } from "./classes/particle.js";
 
 let canvas = document.getElementById("sim-canvas");
 const ctx = canvas.getContext("2d");
@@ -22,35 +23,6 @@ window.addEventListener('resize', resizeCanvas);
 // Element storage
 let elements = [];
 let particles = [];
-
-// Particle class for click effects
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.vx = (Math.random() - 0.5) * 4;
-    this.vy = (Math.random() - 0.5) * 4;
-    this.life = 1.0;
-    this.size = 3;
-    this.color = `hsl(${Math.random() * 60 + 180}, 100%, 50%)`;
-  }
-
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    this.vy += 0.1; // gravity
-    this.life -= 0.02;
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.globalAlpha = this.life;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.globalAlpha = 1.0;
-  }
-}
 
 // ElementBase class
 class ElementBase {
